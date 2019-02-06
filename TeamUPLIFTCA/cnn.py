a9838ff46eb8c2080863cde23080923148996bf4
mkm1088 <35528858+mkm1088@users.noreply.github.com>
2019-02-06 21:14:28 +0800

    Update cnn.py

--- TeamUPLIFTCA/cnn.py
+++ TeamUPLIFTCA/cnn.py
@@ -9,7 +9,7 @@ import re
 
 def NewsFromCNBC():
     #CNBC news api
-    main_url = "https://newsapi.org/v2/everything?language=en&sources=cnn&apiKey=cb28b795dd1e469ebbc02ea19535898a"
+    main_url = "https://newsapi.org/v2/everything?language=en&sources=cnn&apiKey="  " "
 
     # fetching data in json format
     open_cnbc_page = requests.get(main_url).json()

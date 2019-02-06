4f245fa960e49e9882a241366edbea837d0821fb
mkm1088 <35528858+mkm1088@users.noreply.github.com>
2019-02-06 21:18:48 +0800

    Update news.py

--- TeamUPLIFTCA/news.py
+++ TeamUPLIFTCA/news.py
@@ -1,5 +1,5 @@
 from newsapi import NewsApiClient
-newsapi = NewsApiClient(api_key='c44af03c330046fdb4c405c27ceefa95')
+newsapi = NewsApiClient(api_key='*****************')
 top_headlines = newsapi.get_top_headlines(q='bitcoin',
                                           sources='bbc-news,the-verge',
                                           category='business',
